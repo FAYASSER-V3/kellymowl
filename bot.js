76fb2cc94f24a18e1fb7704c123810e7ce9add5d
FAYASSERV5 <97230844+FAYASSERV5@users.noreply.github.com>
2022-01-06 19:15:43 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -133,7 +133,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
         });
 
         console.log(
-            chalk.green.bold('✅ Catbot working!')
+            chalk.green.bold('✅ kellymowl working!')
         );
         await new Promise(r => setTimeout(r, 1100));
 
